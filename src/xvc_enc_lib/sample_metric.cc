fda662786129fac32af7e7674b5f70ae91d74300
Jonatan Samuelsson <jonatan.samuelsson@divideon.com>
2017-03-29 10:28:09 +0200

    Performing shift after the loop in SSE calculation

--- src/xvc_enc_lib/sample_metric.cc
+++ src/xvc_enc_lib/sample_metric.cc
@@ -113,7 +113,7 @@ uint64_t SampleMetric::ComputeSSE(int width, int height,
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
       int diff = sample1[x] - sample2[x];
-#if HM_STRICT
+#if !HM_STRICT
       ssd += (diff*diff) >> shift;
 #else
       ssd += diff * diff;
@@ -122,7 +122,7 @@ uint64_t SampleMetric::ComputeSSE(int width, int height,
     sample1 += stride1;
     sample2 += stride2;
   }
-#if !HM_STRICT
+#if HM_STRICT
   ssd >>= shift;
 #endif
   return ssd;
