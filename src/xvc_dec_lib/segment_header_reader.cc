614715051707b6fab9e8358a96fcec101aa54d2b
Per Hermansson <per.hermansson@divideon.com>
2017-09-22 09:24:01 +0200

    Simplify restriction flag parsing

--- src/xvc_dec_lib/segment_header_reader.cc
+++ src/xvc_dec_lib/segment_header_reader.cc
@@ -77,216 +77,95 @@ Decoder::State SegmentHeaderReader::Read(SegmentHeader* segment_header,
 
   int intra_restrictions = bit_reader->ReadBit();
   if (intra_restrictions) {
-    if (bit_reader->ReadBit()) {
-      restr.disable_intra_ref_padding = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_intra_ref_sample_filter = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_intra_dc_post_filter = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_intra_ver_hor_post_filter = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_intra_planar = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_intra_mpm_prediction = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_intra_chroma_predictor = true;
-    }
+    restr.disable_intra_ref_padding |= !!bit_reader->ReadBit();
+    restr.disable_intra_ref_sample_filter |= !!bit_reader->ReadBit();
+    restr.disable_intra_dc_post_filter |= !!bit_reader->ReadBit();
+    restr.disable_intra_ver_hor_post_filter |= !!bit_reader->ReadBit();
+    restr.disable_intra_planar |= !!bit_reader->ReadBit();
+    restr.disable_intra_mpm_prediction |= !!bit_reader->ReadBit();
+    restr.disable_intra_chroma_predictor |= !!bit_reader->ReadBit();
   }
 
   int inter_restrictions = bit_reader->ReadBit();
   if (inter_restrictions) {
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_mvp = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_scaling_mvp = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_tmvp_mvp = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_tmvp_merge = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_tmvp_ref_list_derivation = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_merge_candidates = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_merge_mode = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_merge_bipred = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_skip_mode = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_chroma_subpel = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_mvd_greater_than_flags = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_inter_bipred = true;
-    }
+    restr.disable_inter_mvp |= !!bit_reader->ReadBit();
+    restr.disable_inter_scaling_mvp |= !!bit_reader->ReadBit();
+    restr.disable_inter_tmvp_mvp |= !!bit_reader->ReadBit();
+    restr.disable_inter_tmvp_merge |= !!bit_reader->ReadBit();
+    restr.disable_inter_tmvp_ref_list_derivation |= !!bit_reader->ReadBit();
+    restr.disable_inter_merge_candidates |= !!bit_reader->ReadBit();
+    restr.disable_inter_merge_mode |= !!bit_reader->ReadBit();
+    restr.disable_inter_merge_bipred |= !!bit_reader->ReadBit();
+    restr.disable_inter_skip_mode |= !!bit_reader->ReadBit();
+    restr.disable_inter_chroma_subpel |= !!bit_reader->ReadBit();
+    restr.disable_inter_mvd_greater_than_flags |= !!bit_reader->ReadBit();
+    restr.disable_inter_bipred |= !!bit_reader->ReadBit();
   }
 
   int transform_restrictions = bit_reader->ReadBit();
   if (transform_restrictions) {
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_adaptive_scan_order = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_residual_greater_than_flags = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_residual_greater2 = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_last_position = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_root_cbf = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_cbf = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_subblock_csbf = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_sign_hiding = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_transform_adaptive_exp_golomb = true;
-    }
+    restr.disable_transform_adaptive_scan_order |= !!bit_reader->ReadBit();
+    restr.disable_transform_residual_greater_than_flags |=
+      !!bit_reader->ReadBit();
+    restr.disable_transform_residual_greater2 |= !!bit_reader->ReadBit();
+    restr.disable_transform_last_position |= !!bit_reader->ReadBit();
+    restr.disable_transform_root_cbf |= !!bit_reader->ReadBit();
+    restr.disable_transform_cbf |= !!bit_reader->ReadBit();
+    restr.disable_transform_subblock_csbf |= !!bit_reader->ReadBit();
+    restr.disable_transform_sign_hiding |= !!bit_reader->ReadBit();
+    restr.disable_transform_adaptive_exp_golomb |= !!bit_reader->ReadBit();
   }
 
   int cabac_restrictions = bit_reader->ReadBit();
   if (cabac_restrictions) {
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_ctx_update = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_split_flag_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_skip_flag_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_inter_dir_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_subblock_csbf_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_coeff_sig_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_coeff_greater1_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_coeff_greater2_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_coeff_last_pos_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_init_per_pic_type = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_cabac_init_per_qp = true;
-    }
+    restr.disable_cabac_ctx_update |= !!bit_reader->ReadBit();
+    restr.disable_cabac_split_flag_ctx |= !!bit_reader->ReadBit();
+    restr.disable_cabac_skip_flag_ctx |= !!bit_reader->ReadBit();
+    restr.disable_cabac_inter_dir_ctx |= !!bit_reader->ReadBit();
+    restr.disable_cabac_subblock_csbf_ctx |= !!bit_reader->ReadBit();
+    restr.disable_cabac_coeff_sig_ctx |= !!bit_reader->ReadBit();
+    restr.disable_cabac_coeff_greater1_ctx |= !!bit_reader->ReadBit();
+    restr.disable_cabac_coeff_greater2_ctx |= !!bit_reader->ReadBit();
+    restr.disable_cabac_coeff_last_pos_ctx |= !!bit_reader->ReadBit();
+    restr.disable_cabac_init_per_pic_type |= !!bit_reader->ReadBit();
+    restr.disable_cabac_init_per_qp |= !!bit_reader->ReadBit();
   }
 
   int deblock_restrictions = bit_reader->ReadBit();
   if (deblock_restrictions) {
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_strong_filter = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_weak_filter = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_chroma_filter = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_boundary_strength_zero = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_boundary_strength_one = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_initial_sample_decision = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_weak_sample_decision = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_two_samples_weak_filter = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_deblock_depending_on_qp = true;
-    }
+    restr.disable_deblock_strong_filter |= !!bit_reader->ReadBit();
+    restr.disable_deblock_weak_filter |= !!bit_reader->ReadBit();
+    restr.disable_deblock_chroma_filter |= !!bit_reader->ReadBit();
+    restr.disable_deblock_boundary_strength_zero |= !!bit_reader->ReadBit();
+    restr.disable_deblock_boundary_strength_one |= !!bit_reader->ReadBit();
+    restr.disable_deblock_initial_sample_decision |= !!bit_reader->ReadBit();
+    restr.disable_deblock_weak_sample_decision |= !!bit_reader->ReadBit();
+    restr.disable_deblock_two_samples_weak_filter |= !!bit_reader->ReadBit();
+    restr.disable_deblock_depending_on_qp |= !!bit_reader->ReadBit();
   }
 
   int high_level_restrictions = bit_reader->ReadBit();
   if (high_level_restrictions) {
-    if (bit_reader->ReadBit()) {
-      restr.disable_high_level_default_checksum_method = true;
-    }
+    restr.disable_high_level_default_checksum_method |=
+      !!bit_reader->ReadBit();
   }
 
   int ext_restrictions = bit_reader->ReadBit();
   if (ext_restrictions) {
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_sink = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_implicit_last_ctu = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_tmvp_full_resolution = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_tmvp_exclude_intra_from_ref_list = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_ref_list_l0_trim = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_implicit_partition_type = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_cabac_alt_split_flag_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_cabac_alt_inter_dir_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_cabac_alt_last_pos_ctx = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_two_cu_trees = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_transform_size_64 = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_intra_unrestricted_predictor = true;
-    }
-    if (bit_reader->ReadBit()) {
-      restr.disable_ext_deblock_subblock_size_4 = true;
-    }
+    restr.disable_ext_sink |= !!bit_reader->ReadBit();
+    restr.disable_ext_implicit_last_ctu |= !!bit_reader->ReadBit();
+    restr.disable_ext_tmvp_full_resolution |= !!bit_reader->ReadBit();
+    restr.disable_ext_tmvp_exclude_intra_from_ref_list |=
+      !!bit_reader->ReadBit();
+    restr.disable_ext_ref_list_l0_trim |= !!bit_reader->ReadBit();
+    restr.disable_ext_implicit_partition_type |= !!bit_reader->ReadBit();
+    restr.disable_ext_cabac_alt_split_flag_ctx |= !!bit_reader->ReadBit();
+    restr.disable_ext_cabac_alt_inter_dir_ctx |= !!bit_reader->ReadBit();
+    restr.disable_ext_cabac_alt_last_pos_ctx |= !!bit_reader->ReadBit();
+    restr.disable_ext_two_cu_trees |= !!bit_reader->ReadBit();
+    restr.disable_ext_transform_size_64 |= !!bit_reader->ReadBit();
+    restr.disable_ext_intra_unrestricted_predictor |= !!bit_reader->ReadBit();
+    restr.disable_ext_deblock_subblock_size_4 |= !!bit_reader->ReadBit();
   }
 
   Restrictions::GetRW() = restr;
